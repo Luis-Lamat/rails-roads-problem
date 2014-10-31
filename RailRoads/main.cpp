c29158d9a9fd95a3c9acf6ee0aa8a20a3637b8c4
Luis-Lamat <beto_alaska@hotmail.com>
2014-10-31 15:32:23 -0600

    Initial Commit

---
+++ RailRoads/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RailRoads
+//
+//  Created by Luis Alberto Lamadrid on 10/31/14.
+//  Copyright (c) 2014 Luis Alberto Lamadrid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
